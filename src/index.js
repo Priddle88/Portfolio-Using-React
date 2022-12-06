9b94b709e8ca3fad87839c2c0b4547aa5f5f34ce
Parker <parkerriddle09@gmail.com>
2022-12-05 23:14:56 -0700

    Added service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -13,3 +14,6 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App tab="home" />);
+
+
+serviceWorkerRegistration.register();

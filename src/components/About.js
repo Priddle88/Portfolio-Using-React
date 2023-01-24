d505701add322c0dec1cc57e11b8c133cb82cf7a
Priddle88 <96860899+Priddle88@users.noreply.github.com>
2023-01-23 21:04:46 -0500

    Added to About page

--- src/components/About.js
+++ src/components/About.js
@@ -9,14 +9,25 @@ function About() {
             <h2 className="aboutTitle">About Me</h2>
             <div className="aboutBox">
                 <img className="pic" alt="Portfolio Pic" src={profPic} />
-                <p className="aboutContent"> I was born in San Diego but moved to Colorado when I was a youngster. I have moved back and forth ever since. I
-                    I have sent the majority in Colorado, but is is hard to pick a favorite. I have four brothers and a sister, and we all love technology in some way.
-                    Well tech is all around us.
-                    My interest in coding began in high school. I found a site called CodeAcademy, which offered free classes in learning how to
-                    code.In my senior year of high school, I was able to take a computer programming course that covered html,
-                    css, and java.
-                    I took a break for a couple of years until I started this fullstack development program with the University of Denver.
+                <div className="aboutParagraphs">
+                <p className="aboutContent"> 
+                &nbsp;&nbsp;&nbsp;&nbsp;My previous work experience is mainly focued on sales and customer service. In high school, I started dabbling in Javascript and HTML/CSS through codeacademy. That led to me taking a class in computer programming during my senior year.
                 </p>
+                <p className="aboutContent"> 
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                Due to a medical condition (that is no longer present), I left midway through my first year of college. Which led me directly into the sales jobs that I mentioned earlier. I chose sales because, at the time, I was having a hard time forming complete thoughts due to the medical condition. I decided that I needed to talk as much as possible until I felt that I had surpassed my previous mental state. That was eight years ago. I was declared fully healed a couple of years ago, which is what brought me back to my passion in coding.
+                </p>
+                <p className="aboutContent"> 
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                I recently completed a Full Stack Web Development Bootcamp through the University of Denver with a 4.0 grade point average (97%), which helped tremendously with gaining knowledge in front-end and back-end development.
+                 I had previous experience with HTML and Javascript but the course showed me that that I was just scaping the surface.
+                 I was taught about using React and databases like MongoDB, simplified CSS with Bootstrap, created logins and used API's. 
+                 </p>
+                 <p className="aboutContent">
+                 &nbsp;&nbsp;&nbsp;&nbsp;
+                 To get more of an idea of what I learned, you can check out my work by clicking on the Portfolio tab at the top of the page. I also attached a link on the Resume page where you can download my resume to see my past work exerience. 
+                </p>
+                </div>
             </div>
         </div>
     );

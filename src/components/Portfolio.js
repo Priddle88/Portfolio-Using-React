f89505302efcc4711f011d1cd33a16af6bd5d20e
Priddle88 <96860899+Priddle88@users.noreply.github.com>
2023-07-13 22:33:54 -0400

    Added new project on portfolio. Added hmtl to open new tab for links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,6 +8,7 @@ import quizImage from "../images/screenshot-quiz.png";
 import monkeyImage from "../images/monkeys-pic.png";
 import pjTwo from "../images/screenshot-project-2.png";
 import githubLogo from "../images/github-logo.png";
+import venue from "../images/venue.png";
 
 function Portfolio() {
 
@@ -17,95 +18,105 @@ function Portfolio() {
             <div className="folio">
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://dpk5e7-team-handbook.herokuapp.com/" >
-                            <h3>Shakespear's Monkeys</h3>
+                        <a href="https://dpk5e7-team-handbook.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                            <h3>
+                                Team Handbook
+                            </h3>
                         </a>
-                        <a href="https://dpk5e7-team-handbook.herokuapp.com/"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/dpk5e7/shakespeares-monkeys" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
-                    <a href="https://dpk5e7-team-handbook.herokuapp.com/" >
+                    <a href="https://dpk5e7-team-handbook.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={monkeyImage} /></a>
                 </div>
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://priddle88.github.io/Password-Generator/" >
+                        <a href="https://alecschro13.github.io/Venue-Search-Project1/" target="_blank" rel="noopener noreferrer">
+                            <h3>
+                                Venue Search
+                            </h3>
+                        </a>
+                        <a href="https://github.com/AlecSchro13/Venue-Search-Project1" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                    </div>
+                    <a href="https://alecschro13.github.io/Venue-Search-Project1/" target="_blank" rel="noopener noreferrer">
+                        <img alt="" className="smApp"
+                            src={venue} /></a>
+                </div>
+                <div className="lilBlock">
+                    <div className="portTitle">
+                        <a href="https://priddle88.github.io/Password-Generator/" target="_blank" rel="noopener noreferrer">
                             <h3>Password Generator</h3>
                         </a>
-                        <a href="https://github.com/Priddle88/Password-Generator"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/Priddle88/Password-Generator" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
-                    <a href="https://priddle88.github.io/Password-Generator/" >
+                    <a href="https://priddle88.github.io/Password-Generator/" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={passwordImage} /></a>
                 </div>
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://priddle88.github.io/Weather-Dashboard/"
-                        >
+                        <a href="https://priddle88.github.io/Weather-Dashboard/" target="_blank" rel="noopener noreferrer">
                             <h3>Weather Dashboard</h3>
                         </a>
-                        <a href="https://github.com/Priddle88/Weather-Dashboard"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/Priddle88/Weather-Dashboard" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
-                    <a href="https://priddle88.github.io/Weather-Dashboard/" >
+                    <a href="https://priddle88.github.io/Weather-Dashboard/" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={weatherImage} /></a>
                 </div>
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://github.com/Priddle88/Work-Profile-Generator"
-                        >
+                        <a href="https://github.com/Priddle88/Work-Profile-Generator" target="_blank" rel="noopener noreferrer">
                             <h3>Work Profile Generator</h3>
                         </a>
-                        <a href="https://github.com/Priddle88/Work-Profile-Generator"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/Priddle88/Work-Profile-Generator" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
-                    <a href="https://github.com/Priddle88/Work-Profile-Generator" >
+                    <a href="https://github.com/Priddle88/Work-Profile-Generator" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={workProfImage} /></a>
                 </div>
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://priddle88.github.io/Work-Day-Scheduler/"
-                        >
+                        <a href="https://priddle88.github.io/Work-Day-Scheduler/" target="_blank" rel="noopener noreferrer">
                             <h3>Work Day Scheduler</h3>
                         </a>
-                        <a href="https://github.com/Priddle88/Work-Day-Scheduler"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/Priddle88/Work-Day-Scheduler" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
-                    <a href="https://priddle88.github.io/Work-Day-Scheduler/" >
+                    <a href="https://priddle88.github.io/Work-Day-Scheduler/" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={schedulerImage} /></a>
                 </div>
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://note-taker-app-pr.herokuapp.com/"
-                        >
+                        <a href="https://note-taker-app-pr.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                             <h3>Note Taker</h3>
                         </a>
-                        <a href="https://github.com/Priddle88/Note-Taker-App"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/Priddle88/Note-Taker-App" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
-                    <a href="https://note-taker-app-pr.herokuapp.com/" >
+                    <a href="https://note-taker-app-pr.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={noteImage} /></a>
                 </div>
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://priddle88.github.io/Quiz/">
+                        <a href="https://priddle88.github.io/Quiz/" target="_blank" rel="noopener noreferrer">
                             <h3>Quiz</h3>
                         </a>
-                        <a href="https://github.com/Priddle88/Quiz"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/Priddle88/Quiz" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
-                    <a href="https://priddle88.github.io/Quiz/" >
+                    <a href="https://priddle88.github.io/Quiz/" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={quizImage} /></a>
                 </div>
                 <div className="lilBlock">
                     <div className="portTitle">
-                        <a href="https://favorite-band-prject2.herokuapp.com/login"
-                        >
+                        <a href="https://favorite-band-prject2.herokuapp.com/login" target="_blank" rel="noopener noreferrer">
                             <h3>Favorite Band Project</h3>
                         </a>
-                        <a href="https://github.com/AlecSchro13/Venue-Search-Project1"><img alt="" className="littleLogo" src={githubLogo} /></a>
+                        <a href="https://github.com/AlecSchro13/Venue-Search-Project1" target="_blank" rel="noopener noreferrer"><img alt="" className="littleLogo" src={githubLogo} /></a>
                     </div>
 
-                    <a href="https://favorite-band-prject2.herokuapp.com/login" >
+                    <a href="https://favorite-band-prject2.herokuapp.com/login" target="_blank" rel="noopener noreferrer">
                         <img alt="" className="smApp"
                             src={pjTwo} /></a>
                 </div>
